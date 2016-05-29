b35d5aa2d36cf5dca2148a74fef861774785b164
Kasia Jarmołkowicz <kasia.jarmolkowicz@gmail.com>
2016-05-29 21:55:44 +0200

    Fix mailers and templates for Łódź 2016

--- app/mailers/mailing.rb
+++ app/mailers/mailing.rb
@@ -1,8 +1,8 @@
 class Mailing < ActionMailer::Base
-  default from: 'railsgirlskrakow@gmail.com'
+  default from: 'railsgirlslodz@gmail.com'
 
   def after_submission(id)
     submission = Submission.find(id)
-    mail(to: submission.email, subject: 'Rails Girls Kraków 2016: Thank you for your submission')
+    mail(to: submission.email, subject: 'Rails Girls Łódź 2016: Thank you for your submission')
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Oceny::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'railsgirlskrakow.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'rails-girls-lodz-2016.herokuapp.com' }
 end
